ebb17546f5d598dd682e3fe30b0eec32dd49883c
u956856 <yjabeur@pca.ma>
2020-10-15 22:02:29 +0100

    Updates

---
+++ precache-manifest.4d0d9a1c3718f546c6a31e6a6515f952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336ee51fd5d8fac57fe9cebe393abcd9",
+    "url": "/jaber111/marocFlixProject/index.html"
+  },
+  {
+    "revision": "2519172891a1ce035c37",
+    "url": "/jaber111/marocFlixProject/static/css/main.e7c155a4.chunk.css"
+  },
+  {
+    "revision": "166e5d6f3cb35c9a07c3",
+    "url": "/jaber111/marocFlixProject/static/js/2.72a3fba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jaber111/marocFlixProject/static/js/2.72a3fba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2519172891a1ce035c37",
+    "url": "/jaber111/marocFlixProject/static/js/main.e2cdc320.chunk.js"
+  },
+  {
+    "revision": "3c1246bbb3be35cf321b",
+    "url": "/jaber111/marocFlixProject/static/js/runtime-main.31f95e15.js"
+  }
+]);
